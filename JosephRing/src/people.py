dba1e8dec02b0dd627764f8f477c3a68e3d69a06
HaoliangFan <85547683+Fanhaoliang723@users.noreply.github.com>
2021-07-12 15:55:13 +0800

    Delete JosephRing directory

--- JosephRing/src/people.py
+++
@@ -1,8 +0,0 @@
-class People(object):
-    def __init__(self, id, name, gender):
-        self.id = id
-        self.name = name
-        self.gender = gender
-    def display_people(self):
-        print(self.id, self.name, self.gender)
-    
