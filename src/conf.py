c0c8a7367eb65f0ebc147918ed3428576c0dcf4e
HaoliangFan <85547683+Fanhaoliang723@users.noreply.github.com>
2021-07-15 17:51:53 +0800

    Delete src directory

--- src/conf.py
+++
@@ -1,3 +0,0 @@
-FILE_PATH = "src\\data.txt"
-LOOP_NUM = 4
-START_NUM = 5
--- src/people.py
+++
@@ -1,8 +0,0 @@
-class People(object):
-    def __init__(self, num, name, gender):
-        self.num = num
-        self.name = name
-        self.gender = gender
-    def display_people(self):
-        print(self.num, self.name, self.gender)
-    
